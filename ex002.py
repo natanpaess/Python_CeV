3a257c1776d1e1e78059da86c70524a9319b7d04
natanpaess <100052433+natanpaess@users.noreply.github.com>
2022-02-28 05:53:25 -0300

    Create ex002.py

---
+++ ex002.py
@@ -0,0 +1,5 @@
+# Faça um programa que leia o nome de uma pessoa e mostre uma 
+# pessoa e mestre uma mensagem de boas vindas.
+
+nome = str(input('Qual é o seu nome? '))
+print(f'Bem-Vindo, {nome}! É um prazer te conhecer!')
